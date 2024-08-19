f7c7822761fb0494add34f83ba3c7521fa4a1320
kevin1010607 <kevin.1010607@gmail.com>
2024-08-19 15:38:09 +0000

    chore: remove k8s & scripts

--- scripts/airflow-clear.sh
+++
@@ -1,6 +0,0 @@
-# Stop port forwarding
-sudo kill -9 $(ps -aux | grep "kubectl port-forward" | grep -v grep | awk '{print $2}' | head -n1)
-
-# Delete the airflow cluster
-kubectl delete ns/airflow --force
-kind delete cluster -n airflow-cluster
